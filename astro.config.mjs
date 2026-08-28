1fd23089e321ad72b78c81212957a917880a4a1f
higorgroto <higorgrotto01gmail.com>
2026-08-28 18:24:08 -0300

    Fixa @astrojs/sitemap na versao 3.2.1 (resolve bug de build)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://higorgroto.github.io',
   base: '/',
   integrations: [
     tailwind(),
+    sitemap(),
   ],
   output: 'static',
   compressHTML: true,
